3ac2d0374c18e99c59b10fdb1d4408ac5d859123
Pallavi-Battu <pallavibattu5@gmail.com>
2025-01-28 18:01:05 +0530

    Add files via upload

---
+++ diamond.java
@@ -0,0 +1,33 @@
+public class diamond {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows for the upper half (can be adjusted)
+
+        // Upper half of the diamond
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+    
+
